124436420327af5fef5067d9479d0683ec043220
Artem Prokofiev <artem-prokofiev@yandex.ru>
2022-11-06 02:23:03 +0400

    create project using mvc

---
+++ Models/ToDo.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Content { get; set; }
+    }
+}
