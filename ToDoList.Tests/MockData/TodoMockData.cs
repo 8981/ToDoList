57e509992add1a39c22d48526cf0dd8cc6abe0fb
Artem Prokofiev <artem-prokofiev@yandex.ru>
2022-12-04 04:12:45 +0400

    added xUnit project for tests

---
+++ ToDoList.Tests/MockData/TodoMockData.cs
@@ -0,0 +1,26 @@
+using ToDoList.Models;
+namespace ToDoList.Tests;
+
+public class TodoMockData
+{
+    public static List<TodoItem> GetToDoItems()
+    {
+        return new List<TodoItem> {
+            new TodoItem {
+                Id = 1,
+                Name = "Walk with dog",
+                IsComplete = true
+            },
+            new TodoItem {
+                Id = 2,
+                Name = "Cocking breakfast",
+                IsComplete = false
+            },
+            new TodoItem {
+                Id = 3,
+                Name = "Go to the shop",
+                IsComplete = true
+            }
+        };
+    }
+}
---
+++ ToDoList.Tests/TestController.cs
@@ -0,0 +1,16 @@
+namespace ToDoList.Tests;
+using System.Threading.Tasks;
+
+public class TestController
+{
+    // [Fact]
+    // public async Task GetTodoItems_ShouldReturn200Status()
+    // {
+    //     ///Arrange
+     
+    //     ///Act
+     
+    //     ///Assert
+        
+    // }
+}
